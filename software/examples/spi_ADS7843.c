19900fdabec1d695330fe768dd167390488edb6d
Vencislav Atanasov <user890104@users.noreply.github.com>
2015-06-14 13:29:32 +0300

    add basic driver for ADS7843

---
+++ software/examples/spi_ADS7843.c
@@ -0,0 +1,99 @@
+/*
+	Updated for the new firmware: July 2012
+	by ihsan Kehribar <ihsan@kehribar.me>
+
+	Created: December 2011
+	by ihsan Kehribar <ihsan@kehribar.me>
+	
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "littleWire.h"
+#include "littleWire_util.h"
+
+unsigned char version;
+unsigned char sendBuffer[4];
+unsigned char receiveBuffer[4];	
+unsigned char *touchX, *touchY;
+
+#define CMD_START 0x80
+#define CMD_A2 0x40
+#define CMD_A1 0x20
+#define CMD_A0 0x10
+#define CMD_MODE_12BIT 0
+#define CMD_MODE_8BIT 0x08
+#define CMD_REF_SINGLE 0x04
+#define CMD_REF_DIFF 0
+#define CMD_PD1 0x02
+#define CMD_PD0 0x01
+
+#define CMD_BASE CMD_START | CMD_MODE_8BIT | CMD_REF_SINGLE
+
+// CALIBRATION DETAILS, TOUCH PANEL SPECIFIC
+// FIND THE EDGE VALUES FOR YOUR PANEL, THEN SET THEM HERE
+#define TS_TOP 0x0c
+#define TS_BOTTOM 0x6f
+#define TS_LEFT 0x0a
+#define TS_RIGHT 0x75
+
+int main()
+{	
+	littleWire *lw = NULL;
+
+	lw = littleWire_connect();
+
+	if (lw == NULL) {
+		printf("> Little Wire could not be found!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	version = readFirmwareVersion(lw);
+	printf("> Little Wire firmware version: %d.%d\n", (version & 0xF0) >> 4, version & 0x0F);
+	
+	if (version == 0x10)
+	{
+		printf("> This example requires the new 1.1 version firmware. Please update soon.\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	spi_init(lw);
+	
+	spi_updateDelay(lw, 0);
+	
+	sendBuffer[0] = CMD_BASE | CMD_A0;
+	sendBuffer[1] = 0;
+	sendBuffer[2] = CMD_BASE | CMD_A2 | CMD_A0;
+	sendBuffer[3] = 0;
+	
+	for(;;)
+	{
+		spi_sendMessage(lw, sendBuffer, receiveBuffer, 4, MANUAL_CS);
+		
+		if (lwStatus < 0)
+		{
+			printf("> lwStatus: %d\n",lwStatus);
+			printf("> Connection error!\n");
+			exit(EXIT_FAILURE);
+		}
+		
+		touchX = receiveBuffer + 1;
+		touchY = receiveBuffer + 3;
+		
+		//printf("0x%02x 0x%02x\n", *touchX, *touchY);
+		
+		if (*touchX == 0x7f && *touchY == 0x00) {
+			continue;
+		}
+		
+		if (*touchX < TS_LEFT || *touchX > TS_RIGHT || *touchY < TS_TOP || *touchY > TS_BOTTOM) {
+			continue;
+		}
+		
+		printf(
+			"X: %03u Y: %03u\n",
+			320 * (*touchX - TS_LEFT) / (TS_RIGHT - TS_LEFT),
+			240 * (*touchY - TS_TOP) / (TS_BOTTOM - TS_TOP)
+		);
+	}
+}
